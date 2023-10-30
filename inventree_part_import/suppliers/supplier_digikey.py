90b3b211985ed15930ae8627e1a5820f9937495f
Bobbe <maxschlecht@web.de>
2023-10-30 03:36:03 +0100

    Add DigiKey and LCSC suppliers

---
+++ inventree_part_import/suppliers/supplier_digikey.py
@@ -0,0 +1,101 @@
+import logging
+import os
+
+import digikey
+from digikey.v3.productinformation import KeywordSearchRequest, ProductDetailsResponse
+from platformdirs import user_cache_path
+
+from .base import ApiPart, Supplier
+from .. import __package__ as parent_package
+
+DIGIKEY_CACHE = user_cache_path(parent_package, ensure_exists=True) / "digikey"
+DIGIKEY_CACHE.mkdir(parents=True, exist_ok=True)
+
+class DigiKey(Supplier):
+    def setup(self, client_id, client_secret, currency, language, location):
+        os.environ["DIGIKEY_CLIENT_ID"] = client_id
+        os.environ["DIGIKEY_CLIENT_SECRET"] = client_secret
+        os.environ["DIGIKEY_CLIENT_SANDBOX"] = "False"
+        os.environ["DIGIKEY_STORAGE_PATH"] = str(DIGIKEY_CACHE)
+
+        self.currency = currency
+        self.language = language
+        self.location = location
+
+        logging.getLogger("digikey.v3.api").setLevel(logging.CRITICAL)
+
+    def search(self, search_term):
+        digikey_part = digikey.product_details(
+            search_term,
+            x_digikey_locale_currency=self.currency,
+            x_digikey_locale_site=self.location,
+            x_digikey_locale_language=self.language,
+        )
+        if digikey_part:
+            return [self.get_api_part(digikey_part)], 1
+
+        results = digikey.keyword_search(
+            body=KeywordSearchRequest(keywords=search_term, record_count=10),
+            x_digikey_locale_currency=self.currency,
+            x_digikey_locale_site=self.location,
+            x_digikey_locale_language=self.language,
+        )
+
+        if results.exact_manufacturer_products_count > 0:
+            filtered_results = results.exact_manufacturer_products
+            product_count = results.exact_manufacturer_products_count
+        else:
+            filtered_results = [
+                digikey_part for digikey_part in results.products
+                if digikey_part.manufacturer_part_number.lower().startswith(search_term.lower())
+            ]
+            product_count = results.products_count
+
+        exact_matches = [
+            digikey_part for digikey_part in filtered_results
+            if digikey_part.manufacturer_part_number.lower() == search_term.lower()
+        ]
+        if len(exact_matches) == 1:
+            filtered_results = exact_matches
+            product_count = 1
+
+        return list(map(self.get_api_part, filtered_results)), product_count
+
+    def get_api_part(self, digikey_part):
+        quantity_available = (
+            digikey_part.quantity_available + digikey_part.manufacturer_public_quantity)
+
+        manufacturer_link = ""
+        if isinstance(digikey_part, ProductDetailsResponse):
+            for media in digikey_part.media_links:
+                if media.media_type == "Manufacturer Product Page":
+                    manufacturer_link = media.url
+                    break
+
+        category_path = [digikey_part.category.value, *digikey_part.family.value.split(" - ")]
+
+        parameters = {
+            parameter.parameter: parameter.value
+            for parameter in digikey_part.parameters
+        }
+
+        price_breaks = {
+            price_break.break_quantity: price_break.unit_price
+            for price_break in digikey_part.standard_pricing
+        }
+
+        return ApiPart(
+            description=digikey_part.product_description,
+            image_url=digikey_part.primary_photo,
+            supplier_link=digikey_part.product_url,
+            SKU=digikey_part.digi_key_part_number,
+            manufacturer=digikey_part.manufacturer.value,
+            manufacturer_link=manufacturer_link,
+            MPN=digikey_part.manufacturer_part_number,
+            quantity_available=quantity_available,
+            packaging=digikey_part.packaging.value,
+            category_path=category_path,
+            parameters=parameters,
+            price_breaks=price_breaks,
+            currency=self.currency,
+        )
---
+++ inventree_part_import/suppliers/supplier_lcsc.py
@@ -0,0 +1,124 @@
+import re
+from requests import Session
+
+from .base import ApiPart, Supplier
+from .scrape import scrape, REQUEST_TIMEOUT
+
+API_BASE_URL = "https://wmsc.lcsc.com/wmsc/"
+CURRENCY_URL     = f"{API_BASE_URL}home/currency?currencyCode={{}}"
+SEARCH_URL       = f"{API_BASE_URL}search/global?keyword={{}}"
+PRODUCT_INFO_URL = f"{API_BASE_URL}product/detail?productCode={{}}"
+
+class LCSC(Supplier):
+    def setup(self, currency):
+        self.currency = currency
+
+    def search(self, search_term):
+        setup = self.get_setup_hook()
+        if not (search_result := scrape(SEARCH_URL.format(search_term), setup_hook=setup)):
+            return [], 0
+
+        result = search_result.json()
+        if product_detail := result["result"]["tipProductDetailUrlVO"]:
+            url = PRODUCT_INFO_URL.format(product_detail["productCode"])
+            if detail_request := scrape(url, setup_hook=setup):
+                return [self.get_api_part(detail_request.json()["result"])], 1
+        elif products := result["result"]["productSearchResultVO"]:
+            filtered_matches = [
+                product for product in products["productList"]
+                if product["productModel"].lower().startswith(search_term.lower())
+            ]
+
+            exact_matches = [
+                product for product in filtered_matches
+                if product["productModel"].lower() == search_term.lower()
+            ]
+            if len(exact_matches) == 1:
+                filtered_matches = exact_matches
+
+            return list(map(self.get_api_part, filtered_matches)), len(filtered_matches)
+
+        return [], 0
+
+    def get_api_part(self, lcsc_part):
+        if not (description := lcsc_part.get("productDescEn")):
+            description = lcsc_part.get("productIntroEn")
+        description = description.strip() if description else ""
+
+        image_url = lcsc_part.get("productImageUrlBig", lcsc_part.get("productImageUrl"))
+        if not image_url and (image_urls := lcsc_part.get("productImages")):
+            for image_url in reversed(image_urls):
+                if "front" in image_url:
+                    break
+
+        if url := lcsc_part.get("url"):
+            url_separator = "/product-detail/"
+            prefix, product_url_id = url.split(url_separator)
+            product_url_id = product_url_id
+            supplier_link = url_separator.join((prefix, cleanup_url_id(product_url_id)))
+        else:
+            product_url_id = cleanup_url_id("_".join((
+                lcsc_part["catalogName"], lcsc_part["title"], lcsc_part["productCode"]
+            )))
+            supplier_link = f"https://www.lcsc.com/product-detail/{product_url_id}.html"
+
+        category_path = []
+        if parent := lcsc_part.get("parentCatalogName"):
+            category_path.append(parent)
+        if category := lcsc_part.get("catalogName"):
+            category_path.append(category)
+
+        parameters = {}
+        if lcsc_parameters := lcsc_part.get("paramVOList"):
+            parameters = {
+                parameter.get("paramNameEn"): parameter.get("paramValueEn")
+                for parameter in lcsc_parameters
+            }
+
+        if package := lcsc_part.get("encapStandard"):
+            parameters["Package Type"] = package
+
+        price_list = lcsc_part.get("productPriceList", [])
+        price_breaks = {
+            price_break.get("ladder"): price_break.get("currencyPrice")
+            for price_break in price_list
+        }
+
+        if price_list:
+            currency = CURRENCY_MAP.get(price_list[0].get("currencySymbol"), self.currency)
+        else:
+            currency = self.currency
+
+        return ApiPart(
+            description=REMOVE_HTML_TAGS.sub("", description),
+            image_url=image_url,
+            supplier_link=supplier_link,
+            SKU=lcsc_part.get("productCode", ""),
+            manufacturer=lcsc_part.get("brandNameEn", ""),
+            manufacturer_link="",
+            MPN=lcsc_part.get("productModel", ""),
+            quantity_available=float(lcsc_part.get("stockNumber", 0)),
+            packaging=REMOVE_HTML_TAGS.sub("", lcsc_part.get("productArrange", "")),
+            category_path=category_path,
+            parameters=parameters,
+            price_breaks=price_breaks,
+            currency=currency,
+        )
+
+    def get_setup_hook(self):
+        def setup_hook(session: Session):
+            session.get(CURRENCY_URL.format(self.currency), timeout=REQUEST_TIMEOUT)
+        return setup_hook
+
+REMOVE_HTML_TAGS = re.compile(r"<.*?>")
+
+CLEANUP_URL_ID_REGEX = re.compile(r"[^\w\d\.]")
+def cleanup_url_id(url):
+    url = url.replace(" / ", "_")
+    url = CLEANUP_URL_ID_REGEX.sub("_", url)
+    return url
+
+CURRENCY_MAP = {
+    "€": "EUR",
+    "US$": "USD",
+}
